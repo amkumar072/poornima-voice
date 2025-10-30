6266b42dc3431d65055bb902ec4fd24a4892c492
amkumar072 <amkumar072@gmail.com>
2025-10-30 00:20:13 +0000

    updated tab 1 message

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -11,9 +11,13 @@ export class Tab2Page {
     .tz(this.dateLocal, 'YYYYMMDDHHmm', 'Asia/Calcutta')
     .toISOString();
 
-  message = `Haapy Anniversary Poornima!!!.
-  You are my life, you are my love.
-  I LOVE YOU so much my baby.`;
+  // message = `Haapy Anniversary Poornima!!!.
+  // You are my life, you are my love.
+  // I LOVE YOU so much my baby.`;
+
+  message = `Happy Anniversary, Poornima!
+  You are the heartbeat of my life and the soul of my love.
+  Thank you for being mine—I love you deeply, endlessly, and always. 💞`;
 
   constructor() {}
 }
